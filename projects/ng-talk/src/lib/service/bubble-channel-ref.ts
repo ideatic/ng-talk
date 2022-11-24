e5757d24ff7a9f1ad9c9a524a528d61961ccbabf
Javier Marín <contacto@ideatic.net>
2022-11-24 16:51:23 +0100

    refactor: rename ng-talk-channels to ng-talk-channel-list

--- projects/ng-talk/src/lib/service/bubble-channel-ref.ts
+++ projects/ng-talk/src/lib/service/bubble-channel-ref.ts
@@ -1,8 +1,10 @@
 import {ApplicationRef, ComponentRef, EventEmitter} from "@angular/core";
 import {NgTalkBubbleChannelComponent} from "../components/ng-talk-bubble-channel/ng-talk-bubble-channel.component";
+import {OverlayRef} from "@angular/cdk/overlay";
 
 export class BubbleChannelRef {
   public onDestroyed = new EventEmitter<BubbleChannelRef>();
+  public overlayRef: OverlayRef;
 
   constructor(private _appRef: ApplicationRef,
               public componentRef: ComponentRef<NgTalkBubbleChannelComponent>) {
@@ -12,6 +14,8 @@ export class BubbleChannelRef {
   public destroy() {
     this._appRef.detachView(this.componentRef.hostView);
     this.componentRef.destroy();
+    this.overlayRef.detach();
+    this.overlayRef.dispose();
 
     this.onDestroyed.emit(this);
   }
