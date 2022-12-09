84c0acbf89ae2a8ec51b4c9107ab3a9770f103af
Javier Marín <contacto@ideatic.net>
2022-12-09 16:29:07 +0100

    style: fix message refs image size

--- projects/ng-talk/src/lib/components/channel/message/ref/ng-talk-channel-message-ref.component.ts
+++ projects/ng-talk/src/lib/components/channel/message/ref/ng-talk-channel-message-ref.component.ts
@@ -11,21 +11,26 @@ import {NgTalkChannelMessageBodyComponent} from "../body/ng-talk-channel-message
     <ng-talk-channel-message-body [message]="message"></ng-talk-channel-message-body>
   `,
   styles: [`
-    :host {
-      display: block;
-      text-align: start;
-      background-color: rgba(0, 0, 0, .05);
-      border-radius: 9px;
-      padding: 7px 12px 10px 11px;
-      border-inline-start: 5px solid #06cf9c;
-    }
+      :host {
+          display: block;
+          text-align: start;
+          background-color: rgba(0, 0, 0, .05);
+          border-radius: 9px;
+          padding: 7px 12px 10px 11px;
+          border-inline-start: 5px solid #06cf9c;
+      }
 
-    ng-talk-channel-message-body {
-      display: block;
-      max-height: 3em;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
+      ng-talk-channel-message-body {
+          display: block;
+          max-height: 3em;
+          overflow: hidden;
+          text-overflow: ellipsis;
+      }
+
+      ng-talk-channel-message-body ::ng-deep img {
+          max-height: 3em;
+          object-fit: contain;
+      }
   `]
 })
 export class NgTalkChannelMessageRefComponent {
