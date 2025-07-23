f1f7624dec7010a5daaf0db073574d8ce5a7b1b1
Javier Marín <contacto@ideatic.net>
2025-07-23 23:43:31 +0200

    refactor: use angular core functions formatNumber() and related instead of pipes

--- projects/ng-talk/src/lib/pipes/relativeDate.pipe.ts
+++ projects/ng-talk/src/lib/pipes/relativeDate.pipe.ts
@@ -1,13 +1,13 @@
-import {DatePipe} from '@angular/common';
-import {inject, LOCALE_ID, Pipe, PipeTransform} from '@angular/core';
-import {NgTalkSettings} from '../components/ng-talk-settings';
-import {daysDiff} from '../utils/utils';
+import { formatDate } from '@angular/common';
+import { inject, LOCALE_ID, Pipe, PipeTransform } from '@angular/core';
+import { NgTalkSettings } from '../components/ng-talk-settings';
+import { daysDiff } from '../utils/utils';
 
 @Pipe({
   name: 'relativeDate'
 })
 export class RelativeDatePipe implements PipeTransform {
-  private _datePipe = new DatePipe(inject(LOCALE_ID));
+  private readonly _locale = inject(LOCALE_ID);
 
   public transform(date: Date, settings: NgTalkSettings): string {
     const now = new Date();
@@ -18,7 +18,7 @@ export class RelativeDatePipe implements PipeTransform {
     } else if (days == -1) {
       return settings.yesterdayText;
     } else {
-      return this._datePipe.transform(date, 'shortDate');
+      return formatDate(date, 'shortDate', this._locale);
     }
   }
 }
